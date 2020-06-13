fe9bae6eca527eca5f627de11b836ca05c851cb9
Daniel Logan <daniel.logan2@outlook.com>
2020-06-12 22:50:36 -0400

    minor styling changes

---
+++ src/main/resources/static/js/cssandjs/actions.js
@@ -0,0 +1,11 @@
+/**
+ * Just for testing out adding javascript to the playerView template
+ */
+
+function showAlert() {
+    alert("The button was clicked!");
+}
+
+function showName(name) {
+    alert("Here's the name: " + name);
+}
